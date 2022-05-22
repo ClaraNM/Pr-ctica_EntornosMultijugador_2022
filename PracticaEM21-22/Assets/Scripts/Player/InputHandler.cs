4c383cdaa7bce1fdddf9c67cff22c7055df76fcf
BAmado753 <65674008+BAmado753@users.noreply.github.com>
2022-05-22 18:13:16 +0200

    Nuevo Input
    Se crea el objeto bala pero no se llega a ver por pantalla

---
+++ PracticaEM21-22/Assets/Scripts/Player/InputHandler.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using Unity.Netcode;
+
+public class InputHandler : NetworkBehaviour
+{
+
+    #region Variables
+
+    // https://docs.unity3d.com/Packages/com.unity.inputsystem@1.3/manual/index.html
+    [SerializeField] InputAction _move;
+    [SerializeField] InputAction _jump;
+    [SerializeField] InputAction _hook;
+    [SerializeField] InputAction _fire;
+    [SerializeField] InputAction _mousePosition;
+
+    // https://docs.unity3d.com/ScriptReference/Events.UnityEvent.html
+    public UnityEvent<Vector2> OnMove;
+    public UnityEvent<Vector2> OnMoveFixedUpdate;
+    public UnityEvent<Vector2> OnMousePosition;
+    public UnityEvent<Vector2> OnHook;
+    public UnityEvent<Vector2> OnHookRender;
+    public UnityEvent OnJump;
+    public UnityEvent OnFire;
+
+    //Para los disparos
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    Vector2 CachedMoveInput { get; set; }
+
+    #endregion
+
+    #region Unity Event Functions
+
+    private void Awake()
+    {
+        _move.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Left", "<Keyboard>/a")
+            .With("Down", "<Keyboard>/s")
+            .With("Right", "<Keyboard>/d");
+
+        _jump.AddBinding("<Keyboard>/space");
+        _hook.AddBinding("<Mouse>/middleButton");
+        _fire.AddBinding("<Mouse>/leftButton");
+        _mousePosition.AddBinding("<Mouse>/position");
+    }
+
+    private void OnEnable()
+    {
+        _move.Enable();
+        _jump.Enable();
+        _hook.Enable();
+        _fire.Enable();
+        _mousePosition.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _move.Disable();
+        _jump.Disable();
+        _hook.Disable();
+        _fire.Disable();
+        _mousePosition.Disable();
+    }
+
+    private void Update()
+    {
+        if (IsLocalPlayer)
+        {
+            CachedMoveInput = _move.ReadValue<Vector2>();
+            var mousePosition = _mousePosition.ReadValue<Vector2>();
+
+            var hookPerformed = _hook.WasPerformedThisFrame();
+            var jumpPerformed = _jump.WasPerformedThisFrame();
+
+            Move(CachedMoveInput);
+            MousePosition(mousePosition);
+
+            // https://docs.unity3d.com/2020.3/Documentation/ScriptReference/Camera.ScreenToWorldPoint.html
+            var screenPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+            if (hookPerformed) { Hook(screenPoint); }
+
+            if (jumpPerformed) { Jump(); }
+            if (_fire.WasPerformedThisFrame()) { Fire(); }
+
+            HookRender(CachedMoveInput);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveFixedUpdate(CachedMoveInput);
+    }
+
+    #endregion
+
+    #region InputSystem Related Methods
+
+    void Move(Vector2 input)
+    {
+        OnMove?.Invoke(input);
+    }
+
+    void MoveFixedUpdate(Vector2 input)
+    {
+        OnMoveFixedUpdate?.Invoke(input);
+    }
+
+    void Jump()
+    {
+        OnJump?.Invoke();
+    }
+
+    void Hook(Vector2 input)
+    {
+        OnHook?.Invoke(input);
+    }
+
+    void HookRender(Vector2 input)
+    {
+        OnHookRender?.Invoke(input);
+    }
+
+    void Fire()
+    {
+        //OnFire?.Invoke();
+
+        //Se crea la bala a partir del prefab
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        //Se añade velocidad a la bala
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6.0f;
+
+        //Se destruye la bala después de 2 segundos
+        Destroy(bullet, 2);
+    }
+
+    void MousePosition(Vector2 input)
+    {
+        OnMousePosition?.Invoke(input);
+    }
+
+    #endregion
+
+}
